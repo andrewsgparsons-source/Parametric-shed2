2779cbc531eeae7e723e61070f1dbd62470fb95d
andrewsgparsons-source <andrewsgparsons@gmail.com>
2026-01-05 21:04:55 +0000

    Update index.js

--- docs/src/index.js
+++ docs/src/index.js
@@ -201,9 +201,7 @@ function initApp() {
 
     var vWallsEl = $("vWalls");
     var vRoofEl = $("vRoof");
-    var vRoofStructureEl = $("vRoofStructure");
-    var vRoofOsbEl = $("vRoofOsb");
-    var vRoofCoveringEl = $("vRoofCovering");
+    var vRoofStructureEl = $("vRoofStructure"), vRoofOsbEl = $("vRoofOsb"), vRoofCoveringEl = $("vRoofCovering");
     var vBaseEl = $("vBase");
     var vFrameEl = $("vFrame");
     var vInsEl = $("vIns");
@@ -294,12 +292,7 @@ function initApp() {
     }
 
     function getRoofEnabled(state) { return (state && state.vis && typeof state.vis.roof === "boolean") ? state.vis.roof : true; }
-
-    function getRoofParts(state) {
-      var vis = state && state.vis ? state.vis : null;
-      var rp = (vis && vis.roofParts && typeof vis.roofParts === "object") ? vis.roofParts : null;
-      return { structure: rp ? rp.structure !== false : true, osb: rp ? rp.osb !== false : true, covering: rp ? rp.covering !== false : true };
-    }
+    function getRoofParts(state){var vis=state&&state.vis?state.vis:null,rp=vis&&vis.roofParts&&typeof vis.roofParts==="object"?vis.roofParts:null;return{structure:rp?(rp.structure!==false):true,osb:rp?(rp.osb!==false):true,covering:rp?(rp.covering!==false):true};}
 
     function getWallParts(state) {
       var vis = state && state.vis ? state.vis : null;
@@ -1380,11 +1373,7 @@ function initApp() {
 
         if (vWallsEl) vWallsEl.checked = getWallsEnabled(state);
         if (vRoofEl) vRoofEl.checked = getRoofEnabled(state);
-
-        var rp0 = getRoofParts(state);
-        if (vRoofStructureEl) vRoofStructureEl.checked = !!rp0.structure;
-        if (vRoofOsbEl) vRoofOsbEl.checked = !!rp0.osb;
-        if (vRoofCoveringEl) vRoofCoveringEl.checked = !!rp0.covering;
+        var rp = getRoofParts(state); if (vRoofStructureEl) vRoofStructureEl.checked = !!rp.structure; if (vRoofOsbEl) vRoofOsbEl.checked = !!rp.osb; if (vRoofCoveringEl) vRoofCoveringEl.checked = !!rp.covering;
 
         var parts = getWallParts(state);
         if (vWallFrontEl) vWallFrontEl.checked = !!parts.front;
@@ -1488,10 +1477,9 @@ function initApp() {
     }
 
     if (vRoofEl) vRoofEl.addEventListener("change", function(e){ store.setState({ vis: { roof: !!e.target.checked } }); console.log("[vis] roof=", !!e.target.checked); });
-
-    if (vRoofStructureEl) vRoofStructureEl.addEventListener("change", function(e){ var on=!!e.target.checked; store.setState({ vis: { roofParts: { structure: on } } }); console.log("[vis] roofParts.structure=", on ? "ON" : "OFF"); });
-    if (vRoofOsbEl) vRoofOsbEl.addEventListener("change", function(e){ var on=!!e.target.checked; store.setState({ vis: { roofParts: { osb: on } } }); console.log("[vis] roofParts.osb=", on ? "ON" : "OFF"); });
-    if (vRoofCoveringEl) vRoofCoveringEl.addEventListener("change", function(e){ var on=!!e.target.checked; store.setState({ vis: { roofParts: { covering: on } } }); console.log("[vis] roofParts.covering=", on ? "ON" : "OFF"); });
+    if (vRoofStructureEl) vRoofStructureEl.addEventListener("change", function(e){ var on = !!(e && e.target && e.target.checked); store.setState({ vis: { roofParts: { structure: on } } }); console.log("[vis] roofParts.structure=", on ? "ON" : "OFF"); });
+    if (vRoofOsbEl) vRoofOsbEl.addEventListener("change", function(e){ var on = !!(e && e.target && e.target.checked); store.setState({ vis: { roofParts: { osb: on } } }); console.log("[vis] roofParts.osb=", on ? "ON" : "OFF"); });
+    if (vRoofCoveringEl) vRoofCoveringEl.addEventListener("change", function(e){ var on = !!(e && e.target && e.target.checked); store.setState({ vis: { roofParts: { covering: on } } }); console.log("[vis] roofParts.covering=", on ? "ON" : "OFF"); });
 
     if (vBaseEl) vBaseEl.addEventListener("change", function (e) { store.setState({ vis: { base: !!e.target.checked } }); });
     if (vFrameEl) vFrameEl.addEventListener("change", function (e) { store.setState({ vis: { frame: !!e.target.checked } }); });
